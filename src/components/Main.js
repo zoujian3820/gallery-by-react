061b6d236e2117f23a34e2a6c231cb7e4a87aa42
mrzou <zjzfbyxz@163.com>
2017-11-03 15:43:55 +0800

    add img lit

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,22 +4,43 @@ require('styles/App.styl');
 import React from 'react';
 
 let imageDatas = require('../data/imageDatas.json')
+
 imageDatas = (function (imageDatasArr) {
-  return imageDatasArr.map(item => item.imageUrl = require(`../images/${item.fileName}`))
+  return imageDatasArr.map((item) => {
+    item.imageUrl = require(`../images/${item.fileName}`);
+    return item;
+  })
 })(imageDatas)
 
-
+let ImgFigure = React.createClass({
+  render() {
+    return (
+      <figure>
+        <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
+        <figcaption>
+          <h2></h2>
+        </figcaption>
+      </figure>
+    );
+  }
+})
 //let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [],
+      imgFigures = [];
+    imageDatas.forEach(function (value,index) {
+        imgFigures.push(<ImgFigure key={index} data={value}/>)
+      }
+    )
     return (
       <section className="stage">
         <section className="img-sec">
-          6
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-
+          {controllerUnits}
         </nav>
       </section>
     );
